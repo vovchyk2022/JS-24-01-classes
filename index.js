239cdb088d3727d34a2b6a07ef22b00cc72a1bd7
Volodymyr Kapushchak <poshta070887@gmail.com>
2023-01-02 22:58:15 +0200

    Add js

---
+++ index.js
@@ -0,0 +1,16 @@
+// alert(123)
+// window.alert(456)
+
+console.log(123);
+console.log(12.3);
+console.log('text');
+console.log(true);
+console.log(null);
+console.log(undefined);
+
+let userName = 'Brad';
+let userAge; //оголошення
+userAge=59; //ініціалізація
+
+console.log(userName);
+
