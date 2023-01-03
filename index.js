2e5b51261aecf629609258747b3ff030ce6373f4
Volodymyr Kapushchak <poshta070887@gmail.com>
2023-01-03 09:01:15 +0200

    Add homework

--- index.js
+++ index.js
@@ -9,19 +9,46 @@ console.log(null);
 console.log(undefined);
 
 let userName = 'Brad';
-let userAge; //оголошення
-userAge=59; //ініціалізація
+// let userAge; //оголошення
+// console.log(typeof userAge);
+// userAge=59; //ініціалізація
 
 console.log(userName);
-console.log(userAge);
+// console.log(userAge);
 
 const userGender = 'male';
+const isUserMale = true;
 //userGender = 'female'; // erroy
 
 const AMOUNT_DAYS_WEEK = 7; // - натуральна константа
 let number = 12;
-let number = 89;
 
+{let number = 89;
 console.log(number);
+}
+
+
+
+
+
+
+let userAge = 25
+console.log('user age:', userAge);
+
+const isUserAdult = true;
+
+const userEmail = 'user@gmail.com';
+console.log('email:', userEmail);
+
+const nameProgrammingLanguage ='C++'
+console.log('programming language:',nameProgrammingLanguage);
+
+const isUserHavePet = false;
+
+const SEAZONS_OF_YEAR = '4'
+console.log('seasons of the year:', SEAZONS_OF_YEAR);
+
+
+
 
 
