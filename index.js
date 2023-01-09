18168b35f1bbdc6462f3aa8b1e727b0cef9f891e
Volodymyr Kapushchak <poshta070887@gmail.com>
2023-01-09 15:47:49 +0200

    Add prompt

--- index.js
+++ index.js
@@ -1,53 +1,19 @@
-// alert(123)
-// window.alert(456)
+// const nameProgrammingLanguage ='C++'
+// console.log('programming language:',nameProgrammingLanguage);
 
-console.log(123);
-console.log(12.3);
-console.log('text');
-console.log(true);
-console.log(null);
-console.log(undefined);
+// const isUserHavePet = false;
 
-let userName = 'Brad';
-// let userAge; //оголошення
-// console.log(typeof userAge);
-// userAge=59; //ініціалізація
+// const COUNTER_SEAZONS_OF_YEAR = '4'
+// console.log('seasons of the year:', SEAZONS_OF_YEAR);
 
-console.log(userName);
-// console.log(userAge);
+const userInput = prompt('Enter number: ', '5');
 
-const userGender = 'male';
-const isUserMale = true;
-//userGender = 'female'; // erroy
 
-const AMOUNT_DAYS_WEEK = 7; // - натуральна константа
-let number = 12;
-
-{let number = 89;
-console.log(number);
+if (userInput=== '' || userInput === null || Number.isNaN(Number(userInput))) {
+  console.log('error')
+}else{
+  console.log(userInput, Number(userInput));
 }
-
-
-
-
-
-
-let userAge = 25
-console.log('user age:', userAge);
-
-const isUserAdult = true;
-
-const userEmail = 'user@gmail.com';
-console.log('email:', userEmail);
-
-const nameProgrammingLanguage ='C++'
-console.log('programming language:',nameProgrammingLanguage);
-
-const isUserHavePet = false;
-
-const SEAZONS_OF_YEAR = '4'
-console.log('seasons of the year:', SEAZONS_OF_YEAR);
-
 
 
 
