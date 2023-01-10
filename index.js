6785f76d9b548faff256bc1af409676c2e650c10
Volodymyr Kapushchak <poshta070887@gmail.com>
2023-01-10 22:42:16 +0200

    Add Number.isInteger

--- index.js
+++ index.js
@@ -1,20 +1,81 @@
-// const nameProgrammingLanguage ='C++'
-// console.log('programming language:',nameProgrammingLanguage);
+// // const side = prompt ('enter side', 4);
+// // const side = '4';
 
-// const isUserHavePet = false;
+// /**
+//  * Get perimetr square
+//  * @param {number} side
+//  * @returns {number} number | NaN;
+//  */
+// const getPerimetrSquare = function (side = 8) {
+//   // const perimetr = side + side + side +side;
+//   if (side <= 0) {
+//     return NaN;
+//   }
+//   // const perimetr = side * 4;
+//   // return perimetr;
+//   return side * 4;
+// };
 
-// const COUNTER_SEAZONS_OF_YEAR = '4'
-// console.log('seasons of the year:', SEAZONS_OF_YEAR);
+// console.log(getPerimetrSquare());
+// console.log(getPerimetrSquare(undefined));
+// console.log(getPerimetrSquare(3));
+// console.log(getPerimetrSquare(-3));
 
-const userInput = prompt('Enter number: ', '5');
+// // /**
+// //  * Is user adult
+// //  * @param {number} age
+// //  * @returns {boolen}
+// //  */
+// // const age = prompt ('enter age', 4);
+// // const isAdult = function (age) {
+// // if (age < 18) {
+// //  return false;
+// // }
+// //   return true;
+// // };
+// // console.log(isAdult());
 
+// /**
+//  *
+//  * @param {number} age > 0
+//  * @param {number} FULL_AGE default value = 18
+//  * @returns {boolean}
+//  */
+// const isAdult = function (age, FULL_AGE = 18) {
+//   return age >= FULL_AGE;
+// };
+// const userAge = 45;
+// console.log(isAdult(17));
+// console.log(isAdult(18));
 
-if (userInput=== '' || userInput === null || Number.isNaN(Number(userInput))) {
-  console.log('error')
-}else{
-  console.log(userInput, Number(userInput));
+// /**
+//  *
+//  * @returns {string | null}
+//  */
+// const getUserInput = function () {
+//   const input = prompt();
+//   if (input) {
+//     return input;
+//   }
+//   return null;
+// };
+// console.log(getUserInput());
+
+const number1 = 47;
+const number2 = 5;
+
+/**
+ *
+ * @param {number} numbe1
+ * @param {number} number2
+ * @returns {boolen}
+ */
+const isMultiplicity = function (numbe1, number2) {
+  return number1 % number2 === 0;
+};
+
+if (Number.isInteger(number1) && Number.isInteger(number2)) {
+  console.log(isMultiplicity(number1, number2));
+} else {
+  console.log("number must be integer");
 }
-
-
-
-
