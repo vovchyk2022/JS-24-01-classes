5046634ddfd41426cf448942944cfc93763f0966
Volodymyr Kapushchak <poshta070887@gmail.com>
2023-01-24 20:48:10 +0200

    Add example old syntax

---
+++ js/old_syntax.js
@@ -0,0 +1,28 @@
+
+"use strict";
+
+function User(firstName, lastName, age){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    User.amount++;
+}
+
+// static
+User.amount = 0;
+User.createTestUser = function(){
+    return new User('Test', 'Testtest', 18)
+}
+
+function UserProto(){
+this.getFullName = function(){
+    return '${this.firstName} ${this.lastName}';
+}
+this.toString = function(){
+    return this.firstName;
+}
+}
+
+User.prototype = new UserProto();
+
+const user = new User('Brad', 'Pitt', 59);
