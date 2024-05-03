f291871ab36f452c379b12a180946ed87c8b0f1f
Volodymyr Kapushchak <poshta070887@gmail.com>
2024-05-03 12:29:32 +0300

    Add example extends at User

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
-
 "use strict";
-function User(firstName, lastName, age){
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    User.amount++;
+/*
+  інкапсуляція
+  успадкування
+  поліморфізм
+*/
+
+class Link {
+  #color;
+  constructor(text, href) {
+    this.text = text;
+    this.href = href;
+    this.#color = "red";
   }
-  //static
-  User.amount = 0;
-  User.createTestUser = function(){
-    return new User('Test', 'Testtest', 18);
+
+  logLink(color) {
+    return `<a href="${this.href}">${this.text} ${this.#color}</a>`;
   }
-  function UserProto(){
-    this.getFullName = function(){
-      return `${this.firstName} ${this.lastName}`;
-    }
-    this.toString = function(){
-      return this.firstName;
-    }
+}
+
+
+class LinkPicture extends Link {
+  constructor(href, pic) {
+    super("", href);
+    this.pic = pic;
   }
-  User.prototype = new UserProto();
-  
+
+  logLink(value) {
+    return super.logLink(123) + `<a href="${this.href}"><img src="${this.pic}"></a>`;
+  }
+}
+
+class LinkNewTab extends Link {
+  constructor(text, href) {
+    super(text, href);
+    this.target = "_blank";
+  }
+}
+
+const linkPic = new LinkPicture("http://", "logo.png");
+console.log(linkPic.logLink("blue"));
+//console.log(linkPic);
+
+const link = new Link("qwerty", "./index.html");
+console.log(link.logLink("blue"));
+//console.log(link);
